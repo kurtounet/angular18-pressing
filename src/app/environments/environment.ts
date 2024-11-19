af903f359cae412d02da1ef1eef631142096108d
Anthony bossut <bossut.anthony@gmail.com>
2024-11-19 12:06:17 +0100

    comment profile address

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
  
 
   addressUrl: 'https://api-adresse.data.gouv.fr/search/',
-  baseUrl: 'https://localhost:8001',
-  baseApiUrl: 'https://localhost:8001/api',
-  authUrl: 'https://localhost:8001/api/login_check',
+  baseUrl: 'https://localhost:8000',
+  baseApiUrl: 'https://localhost:8000/api',
+  authUrl: 'https://localhost:8000/api/login_check',
   assertsImageCategories: '/uploads/images/categories',
   assertsImageServices: '/uploads/images/services',
   assertsImageSite: '/uploads/images/site',
